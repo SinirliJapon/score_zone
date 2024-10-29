ecc392d3cb1a02f8457e1a95dd730c3a63930fbd
SinirliJapon <mkemalerylmz@gmail.com>
2024-10-29 14:13:28 +0100

    Marquee Effect added to Info's

--- lib/presentation/components/custom_info_row.dart
+++ lib/presentation/components/custom_info_row.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 import 'package:score_zone/utils/colors.dart';
 
 class CustomInfoRow extends StatelessWidget {
@@ -13,10 +14,30 @@ class CustomInfoRow extends StatelessWidget {
     return ListTile(
       leading: Icon(icon),
       title: Text(info),
-      trailing: Text(data, style: const TextStyle(fontSize: 16)),
+      trailing: infoTrailing(),
+      leadingAndTrailingTextStyle: const TextStyle(fontSize: 16),
       iconColor: primaryText,
       textColor: primaryText,
       tileColor: secondaryText,
     );
+  }
+
+  Widget infoTrailing() {
+    if (data.length > 20) {
+      return SizedBox(
+        width: 150,
+        child: Marquee(
+          text: data,
+          velocity: 30.0,
+          blankSpace: 20.0,
+          scrollAxis: Axis.horizontal,
+          style: const TextStyle(fontSize: 16, color: primaryText),
+          startAfter: const Duration(seconds: 1),
+          pauseAfterRound: const Duration(seconds: 1),
+        ),
+      );
+    } else {
+      return Text(data);
+    }
   }
 }
